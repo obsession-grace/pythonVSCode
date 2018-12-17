0e4088cdbd43fdc8bf7ab3f4e2ede301ada2b967
Don Jayamanne <don.jayamanne@yahoo.com>
2018-12-17 13:11:45 -0800

    use cache

--- src/test/interpreters/locators/workspaceVirtualEnvService.test.ts
+++ src/test/interpreters/locators/workspaceVirtualEnvService.test.ts
@@ -28,7 +28,7 @@ suite('Interpreters - Workspace VirtualEnv Service', function () {
 
     async function waitForInterpreterToBeDetected(envNameToLookFor: string) {
         const predicate = async () => {
-            const items = await locator.getInterpreters(workspaceUri, true);
+            const items = await locator.getInterpreters(workspaceUri);
             return items.some(item => item.envName === envNameToLookFor && !item.cachedEntry);
         };
         await waitForCondition(predicate, timeoutMs, `${envNameToLookFor}, Environment not detected in the workspace ${workspaceUri.fsPath}`);
