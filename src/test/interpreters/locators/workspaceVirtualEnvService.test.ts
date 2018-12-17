f097c662098c8df8a7a1c1f05a7293b03824cea6
Don Jayamanne <don.jayamanne@yahoo.com>
2018-12-17 13:46:43 -0800

    Clean up code

--- src/test/interpreters/locators/workspaceVirtualEnvService.test.ts
+++ src/test/interpreters/locators/workspaceVirtualEnvService.test.ts
@@ -3,7 +3,7 @@
 
 'use strict';
 
-// tslint:disable:no-any max-classes-per-file max-func-body-length no-invalid-this no-console
+// tslint:disable:no-any max-classes-per-file max-func-body-length no-invalid-this
 import { expect } from 'chai';
 import { exec } from 'child_process';
 import * as path from 'path';
@@ -15,7 +15,7 @@ import { deleteFiles, isPythonVersionInProcess, PYTHON_PATH, rootWorkspaceUri, w
 import { IS_MULTI_ROOT_TEST } from '../../constants';
 import { initialize, multirootPath } from '../../initialize';
 
-const timeoutMs = 180_000;
+const timeoutMs = 60_000;
 suite('Interpreters - Workspace VirtualEnv Service', function () {
     this.timeout(timeoutMs);
     this.retries(1);
@@ -37,23 +37,13 @@ suite('Interpreters - Workspace VirtualEnv Service', function () {
         // Ensure env is random to avoid conflicts in tests (currupting test data).
         const envName = `${venvPrefix}${envSuffix}${new Date().getTime().toString()}`;
         return new Promise<string>((resolve, reject) => {
-            console.log(`Starting to create ${envSuffix} with name ${envName}`);
-            exec(`${PYTHON_PATH.fileToCommandArgument()} -m venv ${envName}`, { cwd: workspaceUri.fsPath }, (ex, stdout, stderr) => {
-                // tslint:disable-next-line:no-console
-                console.log(stdout);
+            exec(`${PYTHON_PATH.fileToCommandArgument()} -m venv ${envName}`, { cwd: workspaceUri.fsPath }, (ex, _, stderr) => {
                 if (ex) {
-                    // tslint:disable-next-line:no-console
-                    console.error(ex);
                     return reject(ex);
                 }
                 if (stderr && stderr.length > 0) {
-                    // tslint:disable-next-line:no-console
-                    console.error(stderr);
-                    const err = new Error(`Failed to create Env ${envName}, ${PYTHON_PATH}, Error: ${stderr}`);
-                    reject(err);
+                    reject(new Error(`Failed to create Env ${envName}, ${PYTHON_PATH}, Error: ${stderr}`));
                 } else {
-                    // tslint:disable-next-line:no-console
-                    console.log(`Created ${envSuffix} with name ${envName}`);
                     resolve(envName);
                 }
             });
@@ -74,42 +64,30 @@ suite('Interpreters - Workspace VirtualEnv Service', function () {
     suiteTeardown(() => deleteFiles(path.join(workspaceUri.fsPath, `${venvPrefix}*`)));
     teardown(() => deleteFiles(path.join(workspaceUri.fsPath, `${venvPrefix}*`)));
 
-    test('Completion of interpreter detection', () => locator.getInterpreters(workspaceUri));
+    test('Completion of interpreter detection', async () => {
+        // This test is required, we need to wait for interpreter listing completes,
+        // before proceeding with other tests.
+        await locator.getInterpreters(workspaceUri);
+    });
 
     test('Detect Virtual Environment', async () => {
-        setTimeout(() => {
-            console.error('About to fail test due to timeout');
-        }, timeoutMs - 30);
-        console.log('start test');
         const envName = await createVirtualEnvironment('one');
-        console.log('created env & waiting');
         await waitForInterpreterToBeDetected(envName);
     });
 
     test('Detect a new Virtual Environment', async () => {
-        setTimeout(() => {
-            console.error('About to fail test due to timeout');
-        }, (timeoutMs * 2) - 30);
-        console.log('start test');
         const env1 = await createVirtualEnvironment('first');
-        console.log('created env & waiting');
-
         await waitForInterpreterToBeDetected(env1);
-        console.log('start test with second env');
 
         // Ensure second environment in our workspace folder is detected when created.
-        console.log('created env 2 & waiting');
         const env2 = await createVirtualEnvironment('second');
         await waitForInterpreterToBeDetected(env2);
-    }).timeout(timeoutMs * 2);
+    });
 
     test('Detect a new Virtual Environment, and other workspace folder must not be affected (multiroot)', async function () {
         if (!IS_MULTI_ROOT_TEST) {
             return this.skip();
         }
-        setTimeout(() => {
-            console.error('About to fail test due to timeout');
-        }, (timeoutMs * 2) - 30);
         // There should be nothing in workspacec4.
         let items4 = await locator.getInterpreters(workspace4);
         expect(items4).to.be.lengthOf(0);
@@ -120,5 +98,5 @@ suite('Interpreters - Workspace VirtualEnv Service', function () {
         // Workspace4 should still not have any interpreters.
         items4 = await locator.getInterpreters(workspace4);
         expect(items4).to.be.lengthOf(0);
-    }).timeout(timeoutMs * 2);
+    });
 });
