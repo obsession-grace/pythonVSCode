c4206287154a7c92739042aabc04904239387542
Don Jayamanne <don.jayamanne@yahoo.com>
2018-12-18 10:57:54 -0800

    Refactor debug configuration provider

--- src/client/debugger/extension/configuration/types.ts
+++ src/client/debugger/extension/configuration/types.ts
@@ -3,10 +3,15 @@
 
 'use strict';
 
-import { Uri } from 'vscode';
+import { CancellationToken, DebugConfiguration, Uri, WorkspaceFolder } from 'vscode';
 
 export const IConfigurationProviderUtils = Symbol('IConfigurationProviderUtils');
 
 export interface IConfigurationProviderUtils {
     getPyramidStartupScriptFilePath(resource?: Uri): Promise<string | undefined>;
+}
+
+export const IDebugConfigurationResolver = Symbol('IDebugConfigurationResolver');
+export interface IDebugConfigurationResolver<T extends DebugConfiguration> {
+    resolveDebugConfiguration(folder: WorkspaceFolder | undefined, debugConfiguration: T, token?: CancellationToken): Promise<T | undefined>;
 }
