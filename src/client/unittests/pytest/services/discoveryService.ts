bdb1fdc1470d64a6d4dbb32a44cbe7d3c10b9765
Derek Keeler <dekeeler@microsoft.com>
2019-03-07 14:24:39 -0800

    Fixups

--- src/client/unittests/pytest/services/discoveryService.ts
+++ src/client/unittests/pytest/services/discoveryService.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+'use strict';
+
 import { inject, injectable, named } from 'inversify';
 import { CancellationTokenSource } from 'vscode';
 import { IServiceContainer } from '../../../ioc/types';
