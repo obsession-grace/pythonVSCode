db9e7a083fb7765b45567169e171915c2d5ef412
Derek Keeler <dekeeler@microsoft.com>
2019-03-13 08:07:39 -0700

    Just comment out the whole thing

--- src/test/unittests/pytest/pytest.testMessageService.test.ts
+++ src/test/unittests/pytest/pytest.testMessageService.test.ts
@@ -1,24 +1,25 @@
+// tslint:disable:copyright-and-strict-header no-useless-files
+
+// This set of tests needs to be updated for the new parsing model we
+// are using. See GH #4735
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-'use strict';
+// 'use strict';
 
-// This set of tests needs to be updated for the new parsing model we
-// are using. See GH #4735 (Commented code below is to keep the tests
-// intact and skipped rather than just removing them).
-
-//import { assert } from 'chai';
-//import * as fs from 'fs';
-//import * as path from 'path';
-import { instance, mock } from 'ts-mockito';
-//import * as typeMoq from 'typemoq';
-import * as vscode from 'vscode';
+// import { assert } from 'chai';
+// import * as fs from 'fs';
+// import * as path from 'path';
+// import { instance, mock } from 'ts-mockito';
+// import * as typeMoq from 'typemoq';
+// import * as vscode from 'vscode';
 // import { EXTENSION_ROOT_DIR } from '../../../client/common/constants';
 // import { ProductNames } from '../../../client/common/installer/productNames';
 // import { Product } from '../../../client/common/types';
-import { ICondaService, IInterpreterService } from '../../../client/interpreter/contracts';
-import { InterpreterService } from '../../../client/interpreter/interpreterService';
-import { CondaService } from '../../../client/interpreter/locators/services/condaService';
+// import { ICondaService, IInterpreterService } from '../../../client/interpreter/contracts';
+// import { InterpreterService } from '../../../client/interpreter/interpreterService';
+// import { CondaService } from '../../../client/interpreter/locators/services/condaService';
 // import { TestResultsService } from '../../../client/unittests/common/services/testResultsService';
 // import { TestsHelper } from '../../../client/unittests/common/testUtils';
 // import { TestFlatteningVisitor } from '../../../client/unittests/common/testVisitors/flatteningVisitor';
@@ -27,22 +28,15 @@ import { CondaService } from '../../../client/interpreter/locators/services/cond
 // import { TestsParser as PyTestsParser } from '../../../client/unittests/pytest/services/parserService';
 // import { TestMessageService } from '../../../client/unittests/pytest/services/testMessageService';
 // import { ILocationStackFrameDetails, IPythonUnitTestMessage, PythonUnitTestMessageSeverity } from '../../../client/unittests/types';
-import { rootWorkspaceUri, updateSetting } from '../../common';
-import {
-    initialize,
-    // initializeTest,
-    IS_MULTI_ROOT_TEST
-} from '../../initialize';
-import { UnitTestIocContainer } from '../serviceRegistry';
-import {
-    //ITestDetails,
-    testScenarios
-} from './pytest_run_tests_data';
+// import { rootWorkspaceUri, updateSetting } from '../../common';
+// import { initialize, initializeTest, IS_MULTI_ROOT_TEST } from '../../initialize';
+// import { UnitTestIocContainer } from '../serviceRegistry';
+// import { ITestDetails, testScenarios } from './pytest_run_tests_data';
 
 // const UNITTEST_TEST_FILES_PATH = path.join(EXTENSION_ROOT_DIR, 'src', 'test', 'pythonFiles', 'testFiles', 'standard');
 // const PYTEST_RESULTS_PATH = path.join(EXTENSION_ROOT_DIR, 'src', 'test', 'pythonFiles', 'testFiles', 'pytestFiles', 'results');
 
-const filterdTestScenarios = testScenarios.filter((ts) => { return !ts.shouldRunFailed; });
+// const filterdTestScenarios = testScenarios.filter((ts) => { return !ts.shouldRunFailed; });
 
 // async function testMessageProperties(message: IPythonUnitTestMessage, expectedMessage: IPythonUnitTestMessage, imported: boolean = false, status: TestStatus) {
 //     assert.equal(message.code, expectedMessage.code, 'IPythonUnitTestMessage code');
@@ -66,13 +60,13 @@ const filterdTestScenarios = testScenarios.filter((ts) => { return !ts.shouldRun
 //     }
 // }
 
-/**
- * Generate a Diagnostic object (including DiagnosticRelatedInformation) using the provided test details that reflects
- * what the Diagnostic for the associated test should be in order for it to be compared to by the actual Diagnostic
- * for the test.
- *
- * @param testDetails Test details for a specific test.
- */
+// /**
+//  * Generate a Diagnostic object (including DiagnosticRelatedInformation) using the provided test details that reflects
+//  * what the Diagnostic for the associated test should be in order for it to be compared to by the actual Diagnostic
+//  * for the test.
+//  *
+//  * @param testDetails Test details for a specific test.
+//  */
 // async function getExpectedLocationStackFromTestDetails(testDetails: ITestDetails): Promise<ILocationStackFrameDetails[]> {
 //     const locationStack: ILocationStackFrameDetails[] = [];
 //     const testFilePath = path.join(UNITTEST_TEST_FILES_PATH, testDetails.fileName);
@@ -108,96 +102,92 @@ const filterdTestScenarios = testScenarios.filter((ts) => { return !ts.shouldRun
 //     return locationStack;
 // }
 
-suite('Unit Tests - PyTest - TestMessageService', () => {
-    let ioc: UnitTestIocContainer;
-    const configTarget = IS_MULTI_ROOT_TEST ? vscode.ConfigurationTarget.WorkspaceFolder : vscode.ConfigurationTarget.Workspace;
-    suiteSetup(async () => {
-        await initialize();
-        await updateSetting('unitTest.pyTestArgs', [], rootWorkspaceUri, configTarget);
-    });
-    function initializeDI() {
-        ioc = new UnitTestIocContainer();
-        ioc.registerCommonTypes();
-        ioc.registerUnitTestTypes();
-        ioc.registerVariableTypes();
-        // Mocks.
-        ioc.registerMockProcessTypes();
-        ioc.serviceManager.addSingletonInstance<ICondaService>(ICondaService, instance(mock(CondaService)));
-        ioc.serviceManager.addSingletonInstance<IInterpreterService>(IInterpreterService, instance(mock(InterpreterService)));
-    }
-    // Build tests for the test data that is relevant for this platform.
-    filterdTestScenarios.forEach((scenario) => {
-        suite(scenario.scenarioName, async () => {
-            //let testMessages: IPythonUnitTestMessage[];
-            suiteSetup(async () => {
-                // await initializeTest();
-                // initializeDI();
-                // // Setup the service container for use by the parser.
-                // const testVisitor = typeMoq.Mock.ofType<ITestVisitor>();
-                // const outChannel = typeMoq.Mock.ofType<vscode.OutputChannel>();
-                // const cancelToken = typeMoq.Mock.ofType<vscode.CancellationToken>();
-                // cancelToken.setup(c => c.isCancellationRequested).returns(() => false);
-                // const wsFolder = typeMoq.Mock.ofType<vscode.Uri>();
-                // const options: TestDiscoveryOptions = {
-                //     args: [],
-                //     cwd: UNITTEST_TEST_FILES_PATH,
-                //     ignoreCache: true,
-                //     outChannel: outChannel.object,
-                //     token: cancelToken.object,
-                //     workspaceFolder: wsFolder.object
-                // };
-                // // Setup the parser.
-                // const testFlattener: TestFlatteningVisitor = new TestFlatteningVisitor();
-                // const testHlp: TestsHelper = new TestsHelper(testFlattener, ioc.serviceContainer);
-                // const parser = new PyTestsParser(testHlp);
-                // const discoveryOutput = fs.readFileSync(path.join(PYTEST_RESULTS_PATH, scenario.discoveryOutput), 'utf8').replace(/\/Users\/donjayamanne\/.vscode\/extensions\/pythonVSCode\/src\/test\/pythonFiles\/testFiles\/noseFiles/g, PYTEST_RESULTS_PATH);
-                // const parsedTests: Tests = parser.parse(discoveryOutput, options);
-                // const xUnitParser = new XUnitParser();
-                // await xUnitParser.updateResultsFromXmlLogFile(parsedTests, path.join(PYTEST_RESULTS_PATH, scenario.runOutput), PassCalculationFormulae.pytest);
-                // const testResultsService = new TestResultsService(testVisitor.object);
-                // testResultsService.updateResults(parsedTests);
-                // const testMessageService = new TestMessageService(ioc.serviceContainer);
-                // testMessages = await testMessageService.getFilteredTestMessages(UNITTEST_TEST_FILES_PATH, parsedTests);
-            });
-            suiteTeardown(async () => {
-                // await ioc.dispose();
-                // await updateSetting('unitTest.pyTestArgs', [], rootWorkspaceUri, configTarget);
-            });
-            scenario.testDetails!.forEach((td) => {
-                suite(td.nameToRun, () => {
-                    // let testMessage: IPythonUnitTestMessage;
-                    // let expectedMessage: IPythonUnitTestMessage;
-                    suiteSetup(async () => {
-                        // let expectedSeverity: PythonUnitTestMessageSeverity;
-                        // if (td.status === TestStatus.Error || td.status === TestStatus.Fail) {
-                        //     expectedSeverity = PythonUnitTestMessageSeverity.Error;
-                        // } else if (td.status === TestStatus.Skipped) {
-                        //     expectedSeverity = PythonUnitTestMessageSeverity.Skip;
-                        // } else {
-                        //     expectedSeverity = PythonUnitTestMessageSeverity.Pass;
-                        // }
-                        // const expectedLocationStack = await getExpectedLocationStackFromTestDetails(td);
-                        // expectedMessage = {
-                        //     code: td.nameToRun,
-                        //     message: td.message,
-                        //     severity: expectedSeverity,
-                        //     provider: ProductNames.get(Product.pytest)!,
-                        //     testTime: 0,
-                        //     status: td.status,
-                        //     locationStack: expectedLocationStack,
-                        //     testFilePath: path.join(UNITTEST_TEST_FILES_PATH, td.fileName)
-                        // };
-                        // testMessage = testMessages.find(tm => tm.code === td.nameToRun)!;
-                    });
-                    test('Message', async function () {
-                        // This set of tests needs to be updated for the new parsing model we
-                        // are using. See GH #4735
-                        // tslint:disable-next-line:no-invalid-this
-                        return this.skip();
-                        // await testMessageProperties(testMessage, expectedMessage, td.imported, td.status);
-                    });
-                });
-            });
-        });
-    });
-});
+// suite('Unit Tests - PyTest - TestMessageService', () => {
+//     let ioc: UnitTestIocContainer;
+//     const configTarget = IS_MULTI_ROOT_TEST ? vscode.ConfigurationTarget.WorkspaceFolder : vscode.ConfigurationTarget.Workspace;
+//     suiteSetup(async () => {
+//         await initialize();
+//         await updateSetting('unitTest.pyTestArgs', [], rootWorkspaceUri, configTarget);
+//     });
+//     function initializeDI() {
+//         ioc = new UnitTestIocContainer();
+//         ioc.registerCommonTypes();
+//         ioc.registerUnitTestTypes();
+//         ioc.registerVariableTypes();
+//         // Mocks.
+//         ioc.registerMockProcessTypes();
+//         ioc.serviceManager.addSingletonInstance<ICondaService>(ICondaService, instance(mock(CondaService)));
+//         ioc.serviceManager.addSingletonInstance<IInterpreterService>(IInterpreterService, instance(mock(InterpreterService)));
+//     }
+//     // Build tests for the test data that is relevant for this platform.
+//     filterdTestScenarios.forEach((scenario) => {
+//         suite(scenario.scenarioName, async () => {
+//             let testMessages: IPythonUnitTestMessage[];
+//             suiteSetup(async () => {
+//                 await initializeTest();
+//                 initializeDI();
+//                 // Setup the service container for use by the parser.
+//                 const testVisitor = typeMoq.Mock.ofType<ITestVisitor>();
+//                 const outChannel = typeMoq.Mock.ofType<vscode.OutputChannel>();
+//                 const cancelToken = typeMoq.Mock.ofType<vscode.CancellationToken>();
+//                 cancelToken.setup(c => c.isCancellationRequested).returns(() => false);
+//                 const wsFolder = typeMoq.Mock.ofType<vscode.Uri>();
+//                 const options: TestDiscoveryOptions = {
+//                     args: [],
+//                     cwd: UNITTEST_TEST_FILES_PATH,
+//                     ignoreCache: true,
+//                     outChannel: outChannel.object,
+//                     token: cancelToken.object,
+//                     workspaceFolder: wsFolder.object
+//                 };
+//                 // Setup the parser.
+//                 const testFlattener: TestFlatteningVisitor = new TestFlatteningVisitor();
+//                 const testHlp: TestsHelper = new TestsHelper(testFlattener, ioc.serviceContainer);
+//                 const parser = new PyTestsParser(testHlp);
+//                 const discoveryOutput = fs.readFileSync(path.join(PYTEST_RESULTS_PATH, scenario.discoveryOutput), 'utf8').replace(/\/Users\/donjayamanne\/.vscode\/extensions\/pythonVSCode\/src\/test\/pythonFiles\/testFiles\/noseFiles/g, PYTEST_RESULTS_PATH);
+//                 const parsedTests: Tests = parser.parse(discoveryOutput, options);
+//                 const xUnitParser = new XUnitParser();
+//                 await xUnitParser.updateResultsFromXmlLogFile(parsedTests, path.join(PYTEST_RESULTS_PATH, scenario.runOutput), PassCalculationFormulae.pytest);
+//                 const testResultsService = new TestResultsService(testVisitor.object);
+//                 testResultsService.updateResults(parsedTests);
+//                 const testMessageService = new TestMessageService(ioc.serviceContainer);
+//                 testMessages = await testMessageService.getFilteredTestMessages(UNITTEST_TEST_FILES_PATH, parsedTests);
+//             });
+//             suiteTeardown(async () => {
+//                 await ioc.dispose();
+//                 await updateSetting('unitTest.pyTestArgs', [], rootWorkspaceUri, configTarget);
+//             });
+//             scenario.testDetails!.forEach((td) => {
+//                 suite(td.nameToRun, () => {
+//                     let testMessage: IPythonUnitTestMessage;
+//                     let expectedMessage: IPythonUnitTestMessage;
+//                     suiteSetup(async () => {
+//                         let expectedSeverity: PythonUnitTestMessageSeverity;
+//                         if (td.status === TestStatus.Error || td.status === TestStatus.Fail) {
+//                             expectedSeverity = PythonUnitTestMessageSeverity.Error;
+//                         } else if (td.status === TestStatus.Skipped) {
+//                             expectedSeverity = PythonUnitTestMessageSeverity.Skip;
+//                         } else {
+//                             expectedSeverity = PythonUnitTestMessageSeverity.Pass;
+//                         }
+//                         const expectedLocationStack = await getExpectedLocationStackFromTestDetails(td);
+//                         expectedMessage = {
+//                             code: td.nameToRun,
+//                             message: td.message,
+//                             severity: expectedSeverity,
+//                             provider: ProductNames.get(Product.pytest)!,
+//                             testTime: 0,
+//                             status: td.status,
+//                             locationStack: expectedLocationStack,
+//                             testFilePath: path.join(UNITTEST_TEST_FILES_PATH, td.fileName)
+//                         };
+//                         testMessage = testMessages.find(tm => tm.code === td.nameToRun)!;
+//                     });
+//                     test('Message', async () => {
+//                         await testMessageProperties(testMessage, expectedMessage, td.imported, td.status);
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
